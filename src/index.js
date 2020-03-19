9191f1924c9250e1a04543d791b2a4284076a5bb
leaf08 <leaf08@ya.ru>
2020-03-19 21:42:40 +0300

    init: typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
+  a = Math.min.apply(null, array);
+  if(a !== Infinity) return a;
   return 0;
 }
 
 exports.max = function max (array) {
+  a = Math.max.apply(null, array);
+  if(a !== -Infinity) return a;
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if(typeof(array) === 'undefined') return 0;
+  if(array.length !== 0) {
+    let sum = 0;
+    for(i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    // console.log(sum / array.length);
+    return sum / array.length;
+  }
   return 0;
 }
